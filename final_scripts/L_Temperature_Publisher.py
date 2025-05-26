795fab76e066a6ae6e5340b10836e9784e1b85ac
hermy17 <hermine.stoettinger@gmail.com>
2025-05-26 21:54:50 +0200

    modify mqtt, update comments, move to final ordner

--- final_scripts/L_Temperature_Publisher.py
+++ final_scripts/L_Temperature_Publisher.py
@@ -2,21 +2,35 @@ import time
 import adafruit_dht
 import board
 import paho.mqtt.client as mqtt
+import logging
 
-# MQTT-Konfiguration
+# MQTT Configuration
 MQTT_BROKER = "192.168.8.137" 
 MQTT_PORT = 1883
 MQTT_TOPIC_TEMP = "/sensor/temperature"
 MQTT_TOPIC_HUM = "/sensor/humidity"
 
-# Initialisiere DHT11 (GPIO4)
+# Initialize temperature sensor DHT11 (GPIO4)
 sensor = adafruit_dht.DHT22(board.D4)
 
-# MQTT-Client initialisieren und verbinden
-client = mqtt.Client()
-client.username_pw_set(username="mqtt-user", password="mqtt")
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-client.loop_start()  # falls du später auch Nachrichten empfangen willst
+# Define MQTT Handlers
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        logging.info("Connected to MQTT Broker successfully")
+    else:
+        logging.error(f"Failed to connect, return code {rc}")
+
+def on_publish(client, userdata, mid):
+    logging.info("Temperature published successfully")
+
+# Initialize MQTT-Client 
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.username_pw_set(username="mqtt-user", password="mqtt")
+
+mqttc.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqttc.loop_start()
 
 try:
     while True:
@@ -29,8 +43,8 @@ try:
                 temperature_f = temperature_c * (9 / 5) + 32
                 humidity = sensor.humidity
                 print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
-                client.publish(MQTT_TOPIC_TEMP, temp)
-                client.publish(MQTT_TOPIC_HUM, hum)
+                mqttc.publish(MQTT_TOPIC_TEMP, temp)
+                mqttc.publish(MQTT_TOPIC_HUM, hum)
             else:
                 print("Sensor liefert keine Werte")
 
@@ -40,5 +54,5 @@ try:
 
 except KeyboardInterrupt:
     print("Beende Programm...")
-    client.loop_stop()
-    client.disconnect()
+    mqttc.loop_stop()
+    mqttc.disconnect()
